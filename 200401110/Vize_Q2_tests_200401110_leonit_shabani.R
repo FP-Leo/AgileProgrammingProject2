86703a16eb77662ac191a789f6dd04e55feaed73
Leonit Shabani <leonitshabani3@gmail.com>
2023-11-24 16:10:33 +0300

    Özellik 2, testleri ile tamamlandı

---
+++ 200401110/Vize_Q2_tests_200401110_leonit_shabani.R
@@ -0,0 +1,76 @@
+library(testthat)
+rm(list = ls())
+source("Vize_Q2_200401110_leonit_shabani.R")
+
+# Q 2-1
+test_that("Global Workspace’de spotify_search_artist adlı bir değişken olmalı.", {
+  expect_true(exists("spotify_search_artist"))
+})
+
+# Q 2-2
+test_that("spotify_search_artist adlı değişkenin tipi “function” olmalı.", {
+  expect_true(is.function(spotify_search_artist))
+})
+
+# Q 2-3
+test_that("spotify_search_artist() herhangi bir artist ismi ile çağrıldığında döndürdüğü çıktı bir liste olmalı.", {
+  result <- spotify_search_artist("Imagine Dragons")
+  expect_true(typeof(result) == "list")
+})
+
+# Q 2-4
+test_that("spotify_search_artist() çağrıldığında döndürdüğü listenin iki elementi olmalı", {
+  result <- spotify_search_artist("Imagine Dragons")
+  expect_true(length(result) == 2)
+})
+
+# Q 2-5
+test_that("spotify_search_artist() çağrıldığında döndürdüğü listenin ilk elementinin ismi status_code olmalı", {
+  result <- spotify_search_artist("Imagine Dragons")
+  expect_true(names(result)[1] == "status_code")
+})
+
+# Q 2-6
+test_that("spotify_search_artist() çağrıldığında döndürdüğü listenin ilk elementinin class’ı numeric olmalı", {
+  result <- spotify_search_artist("Imagine Dragons")
+  expect_true(is.numeric(result[[1]]))
+})
+
+# Q 2-7
+test_that("spotify_search_artist() çağrıldığında döndürdüğü listenin status_code adlı elementinin değeri 200’e eşit olmalı", {
+  result <- spotify_search_artist("Imagine Dragons")
+  expect_true(result[[1]] == 200)
+})
+
+# Q 2-8
+test_that("spotify_search_artist() çağrıldığında döndürdüğü listenin ikinci elementinin ismi search_results olmalı", {
+  result <- spotify_search_artist("Imagine Dragons")
+  expect_true(names(result)[2] == "search_results")
+})
+
+# Q 2-9
+test_that("spotify_search_artist() çağrıldığında döndürdüğü listenin ikinci elementinin class’ı data.frame olmalı", {
+  result <- spotify_search_artist("Imagine Dragons")
+  expect_true(is.data.frame(result[[2]]))
+})
+
+# Q 2-10
+test_that("spotify_search_artist() çağrıldığında döndürdüğü listenin ikinci elementinin iki sütun barındırmalı", {
+  result <- spotify_search_artist("Imagine Dragons")
+  expect_true(ncol(result[[2]]) == 2)
+})
+
+# Q 2-11
+test_that("spotify_search_artist() çağrıldığında döndürdüğü listenin ikinci elementinin sütun isimleri c(“artist”, “id”) olmalı", {
+  result <- spotify_search_artist("Imagine Dragons")
+  expect_identical(names(result[[2]]) == c("artist", "id"), c(T,T))
+})
+
+# Q 2-12
+test_that("spotify_search_artist(\"The Doors\") çağrıldığında döndürdüğü listenin ikinci elementinin birinci satırının \"id\" adlı sütunu \"22WZ7M8sxp5THdruNY3gXt\" olmalı", {
+  result <- spotify_search_artist("The Doors")
+  expect_true(result$search_results$id[1] == "22WZ7M8sxp5THdruNY3gXt")
+})
+
+
+
